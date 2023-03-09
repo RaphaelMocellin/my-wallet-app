b627eba899d870f010016505fe29d3a1d5b932a7
RaphaelMocellin <raphael.mocellin@gmail.com>
2023-03-09 14:45:44 -0300

    Elaborada primeira parte da config do redux

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,3 +3,19 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+const INITIAL_STATE = {
+  user: {
+    email: '', // string que armazena o email da pessoa usuária
+  },
+  wallet: {
+    currencies: [], // array de string
+    expenses: [], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
+    editor: false, // valor booleano que indica de uma despesa está sendo editada
+    idToEdit: 0, // valor numérico que armazena o id da despesa que esta sendo editada
+  },
+};
+
+const rootReducer = (state = INITIAL_STATE) => state;
+
+export default rootReducer;
