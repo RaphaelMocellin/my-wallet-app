f1b64f5f14c076c9c472eb6dea577b1559a975ea
RaphaelMocellin <raphael.mocellin@gmail.com>
2023-03-09 15:01:10 -0300

    Acho que finalizei a primeira config do redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 function App() {
   return <div>Hello, TrybeWallet!</div>;
 }
 
-export default App;
+// const mapStateToProps = () => ({
+// });
+
+export default connect()(App);
